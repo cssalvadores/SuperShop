1fbd75b2b485472cc35b53b6f1e1c2f58af9ce16
Cesar Salvador <Cesar.Salvador.27944@formandos.cinel.pt>
2024-09-05 12:44:35 +0100

    Add and edit images Images on API Products User on API Products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -33,5 +33,18 @@ namespace SuperShop.Data.Entities
         public double Stock { get; set; }
 
         public User User { get; set; }
-    }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://localhost:44370{ImageUrl.Substring(1)}";
+            }
+        }
+    }   
 }
